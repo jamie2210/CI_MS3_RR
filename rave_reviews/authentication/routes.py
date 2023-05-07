bbc779d52d730ba91d95e653a9c2545e2cfaf737
jamie2210 <jamieletts2210@gmail.com>
2023-05-07 08:19:06 +0000

    Feat: begin to build regsiter form and user profile page

--- rave_reviews/authentication/routes.py
+++ rave_reviews/authentication/routes.py
@@ -4,6 +4,15 @@ from flask import (
     redirect, request, session, url_for)
 from werkzeug.security import generate_password_hash, check_password_hash
 from rave_reviews import mongo
+from bson.objectid import ObjectId
+import boto3
+
+UPLOAD_FOLDER = "uploads"
+BUCKET = "rave-review-bucket"
+
+s3 = boto3.client('s3',
+                  aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
+                  aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'])
 
 authentication = Blueprint('authentication', __name__)
 
@@ -19,9 +28,12 @@ def register():
             flash("Username already exists")
             return redirect(url_for("register"))
 
+        image_url = upload("profile_image")
+
         register = {
             "username": request.form.get("username").lower(),
-            "password": generate_password_hash(request.form.get("password"))
+            "password": generate_password_hash(request.form.get("password")),
+            "profile_image": image_url,
         }
         mongo.db.users.insert_one(register)
 
@@ -31,6 +43,29 @@ def register():
         return redirect(
             url_for("authentication.profile", username=session["user"]))
     return render_template("register.html", title="REGISTER")
+
+
+ALLOWED_EXTENSIONS = {'jpg', 'JPG', 'png', 'PNG'}
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+def upload(file_key):
+    f = request.files[file_key]
+    if f and allowed_file(f.filename):
+        file_name = secure_filename(f.filename)
+        file_content = f.read()
+        s3.put_object(
+            Bucket=BUCKET, Key=file_name, Body=file_content,
+            ContentType=f.content_type)
+        image_url = f"https://{BUCKET}.s3.amazonaws.com/{file_name}"
+        return image_url
+    else:
+        flash("Invalid file format. Use 'jpg', 'JPG', 'png', 'PNG'")
+        return redirect(request.url)
 
 
 @authentication.route("/login", methods=["GET", "POST"])
