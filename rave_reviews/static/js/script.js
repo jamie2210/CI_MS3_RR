9bf4dbea8d26c895f6d9b02c9273878378a34254
jamie2210 <jamieletts2210@gmail.com>
2023-05-15 16:45:12 +0000

    Fix: Edit js code

--- rave_reviews/static/js/script.js
+++ rave_reviews/static/js/script.js
@@ -1,5 +1,71 @@
 /*jshint esversion: 6 */
 
+/**
+ * Jquery functions taken from materialize
+ * and code institue tutorials
+ */
+
+$(document).ready(function () {
+    $('.sidenav').sidenav({
+        edge: "right"
+    });
+    $('.carousel').carousel();
+    $('.tooltipped').tooltip();
+    $('select').formSelect();
+    $('.materialboxed').materialbox();
+    $('.collapsible').collapsible();
+    $('.datepicker').datepicker({
+        dateFormat: "dd MM yy",
+        maxDate: null,
+        showClearBtn: true,
+        i18n: {
+            done: "select"
+        }
+    });
+
+    validateMaterializeSelect();
+
+    function validateMaterializeSelect() {
+        let classValid = {
+            "border-bottom": "1px solid #4caf50",
+            "box-shadow": "0 1px 0 0 #4caf50"
+        };
+        let classInvalid = {
+            "border-bottom": "1px solid #f44336",
+            "box-shadow": "0 1px 0 0 #f44336"
+        };
+        if ($("select.validate").prop("required")) {
+            $("select.validate").css({
+                "display": "block",
+                "height": "0",
+                "padding": "0",
+                "width": "0",
+                "position": "absolute",
+                "opacity": "0"
+            });
+        }
+        $(".select-wrapper input.select-dropdown").on("focusin", function () {
+            $(this).parent(".select-wrapper").on("change", function () {
+                if ($(this).children("ul").children("li.selected:not(.disabled)").on("click", function () {})) {
+                    $(this).children("input").css(classValid);
+                }
+            });
+        }).on("click", function () {
+            if ($(this).parent(".select-wrapper").children("ul").children("li.selected:not(.disabled)").css("background-color") === "rgba(0, 0, 0, 0.0)") {
+                $(this).parent(".select-wrapper").children("input").css(classValid);
+            } else {
+                $(".select-wrapper input.select-dropdown").on("focusout", function () {
+                    if ($(this).parent(".select-wrapper").children("select").prop("required")) {
+                        if ($(this).css("border-bottom") != "1px solid rgb(76, 175, 80)") {
+                            $(this).parent(".select-wrapper").children("input").css(classInvalid);
+                        }
+                    }
+                });
+            }
+        });
+    }
+});
+
 document.addEventListener('DOMContentLoaded', function () {});
 
 /**
@@ -118,69 +184,3 @@ closeOrgModal,
 deleteUserModal,
 closeUserModal,
 sendMail */
-
-/**
- * Jquery functions taken from materialize
- * and code institue tutorials
- */
-
-$(document).ready(function () {
-    $('.sidenav').sidenav({
-        edge: "right"
-    });
-    $('.carousel').carousel();
-    $('.tooltipped').tooltip();
-    $('select').formSelect();
-    $('.materialboxed').materialbox();
-    $('.collapsible').collapsible();
-    $('.datepicker').datepicker({
-        dateFormat: "dd MM yy",
-        maxDate: null,
-        showClearBtn: true,
-        i18n: {
-            done: "select"
-        }
-    });
-
-    validateMaterializeSelect();
-
-    function validateMaterializeSelect() {
-        let classValid = {
-            "border-bottom": "1px solid #4caf50",
-            "box-shadow": "0 1px 0 0 #4caf50"
-        };
-        let classInvalid = {
-            "border-bottom": "1px solid #f44336",
-            "box-shadow": "0 1px 0 0 #f44336"
-        };
-        if ($("select.validate").prop("required")) {
-            $("select.validate").css({
-                "display": "block",
-                "height": "0",
-                "padding": "0",
-                "width": "0",
-                "position": "absolute",
-                "opacity": "0"
-            });
-        }
-        $(".select-wrapper input.select-dropdown").on("focusin", function () {
-            $(this).parent(".select-wrapper").on("change", function () {
-                if ($(this).children("ul").children("li.selected:not(.disabled)").on("click", function () {})) {
-                    $(this).children("input").css(classValid);
-                }
-            });
-        }).on("click", function () {
-            if ($(this).parent(".select-wrapper").children("ul").children("li.selected:not(.disabled)").css("background-color") === "rgba(0, 0, 0, 0.0)") {
-                $(this).parent(".select-wrapper").children("input").css(classValid);
-            } else {
-                $(".select-wrapper input.select-dropdown").on("focusout", function () {
-                    if ($(this).parent(".select-wrapper").children("select").prop("required")) {
-                        if ($(this).css("border-bottom") != "1px solid rgb(76, 175, 80)") {
-                            $(this).parent(".select-wrapper").children("input").css(classInvalid);
-                        }
-                    }
-                });
-            }
-        });
-    }
-});
