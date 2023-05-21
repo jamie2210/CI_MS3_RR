4f2cab43ab2d4ddd0dc2c372400a2c22335c28cf
jamie2210 <jamieletts2210@gmail.com>
2023-05-21 11:12:07 +0000

    Feat: Disable debug

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ if os.path.exists("env.py"):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
